ed9d28fdcdab51190814c68454318027da52d574
Pablohassan <sesmanovic@gmail.com>
2021-12-13 17:54:20 +0100

    ajout fichiers projet WCS

---
+++ script.js
@@ -0,0 +1,10 @@
+var sendPopup = document.getEleme
+var overlay = document.getElement
+var btnClose = document.getElemen
+sendPopup.addEventListener("click
+btnClose.addEventListener("click"
+function openModal() {
+  overlay.style.display = "block"
+}
+function closePopup() {
+  overlay.style.display = "none";
