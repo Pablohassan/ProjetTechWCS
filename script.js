d1eec774fc6e4d7b101a4d2f136393d471e71695
Pablohassan <sesmanovic@gmail.com>
2021-12-14 15:01:15 +0100

    ajout fichiers projet WCS

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 var sendPopup = document.getElementById("sendPopup");
 var overlay = document.getElementById("overlay");
 var btnClose = document.getElementById("btnClose");
+var popup = document.getElementById("popup");
 
 sendPopup.addEventListener("click", openModal);
 
 btnClose.addEventListener("click", closePopup);
+
+overlay.addEventListener("click", closePopup);
+
+popup.addEventListener("click", noClose); //
 
 function openModal() {
   overlay.style.display = "block";
 }
 function closePopup() {
   overlay.style.display = "none";
+}
+//Quand on click sur la popup on ne veux pas que la popup se ferme
+function noClose(event) {
+  event.stopPropagation();
 }
